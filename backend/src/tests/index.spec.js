810f0edb268b0daef57f30cca9857696a8bf0ced
alu0101130675 <alu0101130675@ull.edu.es>
2022-06-15 19:07:02 +0100

    Probando database

--- backend/src/tests/index.spec.js
+++ backend/src/tests/index.spec.js
@@ -1,9 +1,10 @@
-import app from '../appjs';
-import request from 'supertest';
+const app = require('../app');
+const request = require('supertest');
 
 describe('App', () => {
     test('should respond with 200 status code', async () => {
         const response = await request(app).get('/');
+        console.log(response)
         expect(response.statusCode).toBe(200);
     }
     );
