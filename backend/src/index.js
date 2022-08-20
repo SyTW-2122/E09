f4f188901db700ff08b63040a0168cc056556cf0
AitorAlonsoMelian <alu0101122496@ull.edu.es>
2022-08-20 17:50:45 +0100

    Change ingex.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 require('./database')
 
 //start the server
-app.listen(app.get('port'), () => {
+app.listen((process.env.PORT || app.get('port')), () => {
     console.log(`Example app listening at http://localhost:${app.get('port')}`);
 })
 
