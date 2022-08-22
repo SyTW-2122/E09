9a0b8ec4979ebd6e3ff257054f5dcd86bd758eee
alu0101130675 <alu0101130675@ull.edu.es>
2022-08-22 23:26:38 +0100

    transacton test

--- backend/src/tests/index.spec.js
+++ backend/src/tests/index.spec.js
@@ -3,6 +3,25 @@ const mongoose = require("mongoose");
 const supertest = require('supertest');
 const api = supertest(app)
 const server= require('../index')
+
+const  transacciones = [
+    {
+        nombreUsuario: "Juan",
+        nombreMoneda: "BTC",
+        cantidad: 5,
+        precio: 10,
+        tipo: "compra",
+        fecha: "10-5-22",
+      },
+      {
+        nombreUsuario: "pedro",
+        nombreMoneda: "ETH",
+        cantidad: 7,
+        precio: 100,
+        tipo: "compra",
+        fecha: "10-5-22",
+      },
+]
 //start the server
 /*const server = app.listen(app.get('port'), () => {
     console.log(`Example app listening at http://localhost:${app.get('port')}`);
@@ -22,10 +41,14 @@ describe('App', () => {
         expect(response.statusCode).toBe(200);
     }
     );
-    test('should respond with 200 status code and json', async () =>{
+    test('get transacitions should respond with 200 status code and json', async () =>{
         await api.get('/transactions')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+    })
+    test('post transaction should respond with 200 status code and json', async () =>{
+         await api.post('/transactions').send(transacciones[0])
+        .expect(200)
     })
 })
 afterAll(()=>{
