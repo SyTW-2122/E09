4f0179ef2dae2020fb05e8b5f514544de93b22f2
AitorAlonsoMelian <alu0101122496@ull.edu.es>
2022-08-23 12:38:36 +0100

    Limpieza de código

--- backend/src/controllers/Transactions.controllers.js
+++ backend/src/controllers/Transactions.controllers.js
@@ -10,22 +10,18 @@ TransactionsCtrl.getTransactions = async (req, res) => {
 TransactionsCtrl.createTransaction = async (req, res) => { 
     const newTransaction = new Transaction(req.body)
     await newTransaction.save()
-    console.log(newTransaction)
     res.send(`This object was created:  ${newTransaction}`) 
 }
 TransactionsCtrl.getTransaction = async (req, res) => { 
     const transaction = await Transaction.findById(req.params.nombreUsuario)
-    console.log(req.params.nombreUsuario)
     res.json(transaction) 
 }
 TransactionsCtrl.updateTransaction = async (req, res) => { 
     const transaction = await Transaction.findByIdAndUpdate(req.params.nombreUsuario, req.body)
-    console.log(req.params.id)
     res.json(`Se ha actualizado el objeto con id: ${req.params.id}`)
 }
 TransactionsCtrl.deleteTransaction = async (req, res) => { 
     const transaction = await Transaction.findByIdAndDelete(req.params.nombreUsuario)
-    console.log(req.params.id)
     res.json(`Se ha eliminado el siguiente objeto: ${transaction}`)
 }
 module.exports = TransactionsCtrl
--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose"); 
-const monedaModel = require("./models/moneda")
-const transaccionModel = require("./models/transaccion")
-const userModel = require("./models/usuario")
 
 const uri = "mongodb+srv://usuario:usuario@cluster0.a1unt.mongodb.net/portfolios?retryWrites=true&w=majority";
 mongoose.connect(uri).then(()=>{
@@ -10,37 +7,3 @@ mongoose.connect(uri).then(()=>{
   console.error(err)
 })
   
-/*
-  //Prueba de database
-  moneda1 = new monedaModel({
-    nombre: "pvu",
-    precio: 0,
-    supply: 1000,
-  })
-compra1= new transaccionModel({
-  tipo: "compra",
-  moneda: moneda1,
-  precio: 200,
-})
-venta2=new transaccionModel({
-  tipo: "venta",
-  moneda: moneda1,
-  precio: 200,
-})
-usuario= new userModel({
-  compras: [compra1,compra1,compra1,compra1],
-  ventas: [venta2],
-  monedas_activas: [moneda1]
-})
-
-  usuario.save()
-  .then(result => {
-    console.log(result)
-  })
-  .catch(err => {
-    console.log(err)
-  })
- 
-console.log(moneda1.precio)
-mongoose.connection.close()
-*/
