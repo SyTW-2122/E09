28ff4a9fb4e2bf0f841d269a4a600ffdc1c5d2e0
AitorAlonsoMelian <alu0101122496@ull.edu.es>
2022-08-26 13:23:25 +0100

    Mejoras en el script de CMC

--- backend/src/cmc-api/CMC.js
+++ backend/src/cmc-api/CMC.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 const monedaModel = require("../models/moneda")
+//require('../database')
 
-const interval = setInterval(function() {
-  let response = null;
+let response = null;
+setInterval(function() {
   new Promise(async (resolve, reject) => {
     try {
       response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC,ETH,USDT,BNB,XRP,ADA,SOL,DOGE,DOT,SHIB,AVAX,MATIC,TRX,UNI,LTC,ATOM,LINK,XMR,XLM,ALGO', {
@@ -21,21 +22,57 @@ const interval = setInterval(function() {
       console.log("La petición a la API de CoinMarketCap ha sido realizada.")
       const monedas = response.data["data"];
       for (let moneda in monedas){
-        const monedaBD = await monedaModel.find({"nombre": monedas[moneda]["symbol"]})
+        console.log("Hola")
+        const monedaBD = await monedaModel.find({"nombre": monedas[moneda]["name"]})
+        //console.log("Moneda: ", monedaBD)
         if (monedaBD.length == 0) {
           // Se crea una nueva moneda en la BD
-          nuevaMoneda = new monedaModel({ nombre: monedas[moneda]["symbol"], precio: monedas[moneda]["quote"]["USD"]["price"], supply: monedas[moneda]["circulating_supply"] })
+          nuevaMoneda = new monedaModel({ 
+            nombre: monedas[moneda]["name"],
+            symbol: monedas[moneda]["symbol"],
+            p1h: monedas[moneda]["quote"]["USD"]["percent_change_1h"],
+            p24h: monedas[moneda]["quote"]["USD"]["percent_change_24h"],
+            p7d: monedas[moneda]["quote"]["USD"]["percent_change_7d"],
+            p30d: monedas[moneda]["quote"]["USD"]["percent_change_30d"],
+            p60d: monedas[moneda]["quote"]["USD"]["percent_change_60d"],
+            p90d: monedas[moneda]["quote"]["USD"]["percent_change_90d"],
+            volumen24h: monedas[moneda]["quote"]["USD"]["volume_24h"],
+            volumenchange24h: monedas[moneda]["quote"]["USD"]["volume_change_24h"],
+            precio: monedas[moneda]["quote"]["USD"]["price"],
+            circulatingSupply: monedas[moneda]["circulating_supply"],
+            maxsupply: monedas[moneda]["max_supply"],
+            marketcap: monedas[moneda]["quote"]["USD"]["market_cap"],
+            marketdom: monedas[moneda]["quote"]["USD"]["market_cap_dominance"],
+            tags: monedas[moneda]["tags"]
+          })
           await nuevaMoneda.save()
         }
         else {
           // Se actualiza la moneda ya existente en la BD
-          nuevaMoneda = { nombre: monedas[moneda]["symbol"], precio: monedas[moneda]["quote"]["USD"]["price"], supply: monedas[moneda]["circulating_supply"] }
-          await monedaModel.findOneAndReplace({nombre: monedas[moneda]["symbol"]}, nuevaMoneda)
+          nuevaMoneda = { 
+            nombre: monedas[moneda]["name"],
+            symbol: monedas[moneda]["symbol"],
+            p1h: monedas[moneda]["quote"]["USD"]["percent_change_1h"],
+            p24h: monedas[moneda]["quote"]["USD"]["percent_change_24h"],
+            p7d: monedas[moneda]["quote"]["USD"]["percent_change_7d"],
+            p30d: monedas[moneda]["quote"]["USD"]["percent_change_30d"],
+            p60d: monedas[moneda]["quote"]["USD"]["percent_change_60d"],
+            p90d: monedas[moneda]["quote"]["USD"]["percent_change_90d"],
+            volumen24h: monedas[moneda]["quote"]["USD"]["volume_24h"],
+            volumenchange24h: monedas[moneda]["quote"]["USD"]["volume_change_24h"],
+            precio: monedas[moneda]["quote"]["USD"]["price"],
+            circulatingSupply: monedas[moneda]["circulating_supply"],
+            maxsupply: monedas[moneda]["max_supply"],
+            marketcap: monedas[moneda]["quote"]["USD"]["market_cap"],
+            marketdom: monedas[moneda]["quote"]["USD"]["market_cap_dominance"],
+            tags: monedas[moneda]["tags"]
+          }
+          await monedaModel.findOneAndReplace({nombre: monedas[moneda]["name"]}, nuevaMoneda)
         }
       }
 
-      //console.log(monedas);
+      //console.log(JSON.stringify(monedas));
       resolve(monedas);
     }
   });
-}, 300000)
+}, 10000)
--- backend/src/models/moneda.js
+++ backend/src/models/moneda.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose"); 
  const monedaSchema = mongoose.Schema({
-  nombre: { type: String, required: true, unique: true},
-  precio: { type: Number, required: true},
-  supply: { type: Number, required: true},
+  nombre:            { type: String, required: true, unique: true},
+  symbol:            { type: String, required: true},
+  p1h:               { type: Number, required: true},
+  p24h:              { type: Number, required: true},
+  p7d:               { type: Number, required: true},
+  p30d:              { type: Number, required: true},
+  p60d:              { type: Number, required: true},
+  p90d:              { type: Number, required: true},
+  volumen24h:        { type: Number, required: true},
+  volumenchange24h:  { type: Number, required: true},
+  precio:            { type: Number, required: true},
+  circulatingSupply: { type: Number, required: true},
+  maxsupply:         { type: Number, required:false},
+  marketcap:         { type: Number, required: true},
+  marketdom:         { type: Number, required: true},
+  tags:              { type: Array, required: true}
+
 })
 const monedaModel= mongoose.model('Moneda',monedaSchema) 
 module.exports = monedaModel
