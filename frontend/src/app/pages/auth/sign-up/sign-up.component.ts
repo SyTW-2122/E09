88260a5dad5603cdbedfd3f4895094dbaef6770a
alu0101130675 <alu0101130675@ull.edu.es>
2022-08-27 19:53:50 +0100

    cambios de formulario

--- frontend/src/app/pages/auth/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/auth/sign-up/sign-up.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
-  template: `<app-form><app-form>`,
+  templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
--- frontend/src/app/pages/auth/sign-up/sign-up.module.ts
+++ frontend/src/app/pages/auth/sign-up/sign-up.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormModule } from 'src/app/pages/auth//form/form.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { SignUpRoutingModule } from './sign-up-routing.module';
 import { SignUpComponent } from './sign-up.component';
@@ -14,7 +14,11 @@ import { SignUpComponent } from './sign-up.component';
   imports: [
     CommonModule,
     SignUpRoutingModule,
-    FormModule
+    ReactiveFormsModule,
+    SharedModule
+  ],
+  exports: [
+    SignUpComponent
   ]
 })
 export class SignUpModule { }
