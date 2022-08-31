9c8cd8e21c02cf921f65e3e8d7584f67288f94cb
alu0101130675 <alu0101130675@ull.edu.es>
2022-08-31 20:35:37 +0100

    get transacction acabado

--- backend/src/controllers/Transactions.controllers.js
+++ backend/src/controllers/Transactions.controllers.js
@@ -16,21 +16,18 @@ TransactionsCtrl.createTransaction = async (req, res) => {
 }
 TransactionsCtrl.getTransaction = async (req, res) => {
     const transactions = await Transaction.find({ "nombreUsuario": req.params.nombre})
+    const monedasBD = await Moneda.find({})
     let monedasDeUsuario = [];
     let comprasGenerales = 0;
     let ventasGenerales = 0;
     let transactionsResult = []
     transactions.forEach(async transactionItem => {
         if (transactionItem.tipo == "compra") { // Suma cantidades que has comprado
-            console.log("compras")
             comprasGenerales += transactionItem.cantidad * transactionItem.precio
         } else { // Suma cantidades que has vendido
-            console.log("ventas")
-
             ventasGenerales += transactionItem.cantidad * transactionItem.precio
         }
-        // Comprueba si la moneda ya ha sido calculada
-        
+        // Comprueba si la moneda ya ha sido calculada       
           if (!monedasDeUsuario.includes(transactionItem.nombreMoneda)) {
             let cantidad = 0;
             let compras = 0;
@@ -49,30 +46,25 @@ TransactionsCtrl.getTransaction = async (req, res) => {
                     // creamos el JSON con los datos que contendra nuestra respuesta
                 }
             }) //cierre for moneda individual
-            const monedaBD = await Moneda.find({ "symbol": transactionItem.nombreMoneda})
-            //console.log(monedaBD)
-            const precio_actual = monedaBD[0].precio
-            //console.log(precio_actual)
+            index = monedasBD.findIndex(element => element.symbol == transactionItem.nombreMoneda)
+            const precio_actual = monedasBD[index].precio
+            let rendimiento = compras + ventas + precio_actual*cantidad
             let objeto = {
                 "nombreMoneda": transactionItem.nombreMoneda,
-                "rendimiento": compras + ventas + precio_actual*cantidad,
-                "cantidad": cantidad
+                "rendimiento": rendimiento,
+                "cantidad": cantidad,
+                "porcentajeRendimiento": -(rendimiento/compras * 100)   //(((ventas + precio_actual * cantidad) / -compras)*100)- 100
             }
             console.log(objeto)
             transactionsResult.push(objeto)
-
         }
     })
-  
-    // console.log("comprasGenerales", comprasGenerales)
-    // console.log("ventasGenerales", ventasGenerales)
     /**Rendimiento de cada moneda */
     let respuesta = {
         "comprasGenerales":comprasGenerales,
         "ventasGenerales":ventasGenerales,
         "transactionsResult":transactionsResult
     }
-    console.log ("resputa",respuesta)
     res.json(respuesta)
 }
 TransactionsCtrl.updateTransaction = async (req, res) => {
